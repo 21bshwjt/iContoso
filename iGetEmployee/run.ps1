6cd95089f5d029de4049426220117fc22bf62d7b
Biswajit Biswas <bshwjt@gmail.com>
2023-11-28 00:32:01 +0530

    removed storage func

--- iGetEmployee/run.ps1
+++ iGetEmployee/run.ps1
@@ -1,23 +1,7 @@
 using namespace System.Net
 param($Request, $TriggerMetadata)
 
-# Replace these values with your actual Azure Storage Account details
-$storageAccountName = "fnstorageacct01"
-$containerName = "testcon"
-$blobName = "yourfile.json"
-$resourceGroupName = "dadaops_rg"
-
-# Get the storage account key
-$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName)[0].Value
-
-# Create a storage context
-$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
-
-# Get the JSON content from the blob
-$jsonContent = Get-AzStorageBlobContent -Container $containerName -Blob $blobName -Context $ctx
-
-
-$employees = Get-Content $jsonContent.Name | ConvertFrom-Json
+$employees = Get-Content .\database\employees.json | ConvertFrom-Json
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         headers    = @{'content-type' = 'application\json' }
         StatusCode = [httpstatuscode]::OK
